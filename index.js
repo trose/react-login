338e5aa9a9da34201819c89d93896ea41360b281
Taylor Rose <taylor.rose@trackvia.com>
2016-03-15 22:35:57 -0600

    applied some polish

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 'use strict';
 
-var Login = React.createClass({
-    render: function() {
-        var loginStyle = {
-            background: '#423142',
-            position: 'absolute',
-            width: '230px',
-            height: '115px',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            borderRadius: '2px'
-        };
+import { createStore } from 'redux';
 
-        var usernameStyle = {
-            marginBottom: '10px',
-            height: '35px',
-            borderRadius: '2px'
-        };
+const loginStore = (state = [], action) => {
+  switch (action.type) {
+    case 'login':
+        state.loading = true;
+        state.error = false;
+        return state;
+    case 'login_finished':
+        state.loading = false;
+        state.error = true;
+        return state;
+    case 'clear_error':
+        state.error = false;
+        state.loading = false;
+        return state;
+    default:
+      return state;
+  }
+};
+
+var store = createStore(loginStore);
+
+var Login = React.createClass({
+    componentDidMount: function() {
+        this.unsubscribe = store.subscribe(this.handleStoreChange);
+    },
+
+    handleStoreChange: function() {
+        this.setState(store.getState());
+    },
+
+    componentWillUnmount: function() {
+        this.unsubscribe();
+    },
+
+    clickHandler: function() {
+        store.dispatch({type: 'login'});
+
+        setTimeout(function() {
+            store.dispatch({type: 'login_finished'});
+        }, 1000);
+
+        setTimeout(function() {
+            store.dispatch({type: 'clear_error'});
+        }, 4000);
+    },
+
+    render: function() {
+        var errorMessage;
+        var gradientClass = 'gradientScroll';
 
         var iconStyle = {
             textAlign: 'center',
@@ -25,37 +58,6 @@ var Login = React.createClass({
             top: '50%',
             transform: 'translateY(-50%)'
         }
-
-        var inputStyle = {
-            color: '#9c9c9c',
-            border: 'none',
-            height: '35px',
-            display: 'inline-block',
-            padding: '0 0 0 7px',
-            fontSize: '11px',
-            WebkitAppearance: 'none',
-            outline: 'none'
-        };
-
-        var submitButtonStyle = {
-            display: 'inline-block',
-            height: '35px',
-            width: '35px',
-            color: '#FEFEFE',
-            border: 'none',
-            borderRadius: '0 2px 2px 0',
-            background: '#AD6BAD'
-        };
-
-        var usernameInputStyle = Object.assign({}, inputStyle, {
-            borderRadius: '0 2px 2px 0',
-            width: 'calc(100% - 35px)'
-        });
-
-        var passwordInputStyle = Object.assign({}, inputStyle, {
-            width: 'calc(100% - 70px)',
-            verticalAlign: 'top'
-        });
 
         var indicatorStyle = {
             display: 'inline-block',
@@ -68,58 +70,69 @@ var Login = React.createClass({
             background: '#DEDEDE'
         }
 
-        var arrowStyle = {
-            width: 0,
-            height: 0,
-            borderLeft: '7px solid transparent',
-            borderRight: '7px solid transparent',
-            borderTop: '7px solid ' + loginStyle.background,
-            position: 'absolute',
-            top: loginStyle.height,
-            left: '17px'
-        };
-
-        var subtextStyle = {
-            position: 'absolute',
-            width: loginStyle.width,
-            top: loginStyle.height,
-            padding: '16px',
-            color: '#594F5F'
-        }
-
-        var forgotStyle = {
-            fontSize: '11px',
-            textDecoration: 'underline',
-            verticalAlign: 'baseline',
-            float: 'right'
-        }
-
         var passwordIndicatorStyle = Object.assign({}, indicatorStyle, {})
 
-        return (<div style={loginStyle}>
-            <div className="gradientScroll" style={{background: '#AABBCC', height: '3px'}}/>
+        var buttonIcon = <div className="icon-arrow-right" style={iconStyle} />;
+
+        if (store.getState().loading) {
+            var loadingIconStyle = {
+                textAlign: 'center',
+                marginTop: '9px',
+                animationName: 'spin',
+                animationDuration: '1000ms',
+                animationIterationCount: 'infinite',
+                animationTimingFunction: 'linear'
+            };
+            buttonIcon = <div className="icon-loop2" style={loadingIconStyle} />;
+
+            gradientClass += ' animate';
+        }
+        var indicatorIcon = <div className="icon-user" style={iconStyle} />;
+
+        if (store.getState().error) {
+            var leftArrowStyle = {
+                width: 0,
+                height: 0,
+                borderTop: '6px solid transparent',
+                borderBottom: '6px solid transparent',
+                borderRight: '6px solid #E62163',
+                position: 'absolute',
+                top: '11px',
+                left: '-6px'
+            }
+
+            indicatorStyle.background = '#E62163';
+            var errorIconStyle = Object.assign({}, iconStyle, {color: 'white'});
+
+            indicatorIcon = <div className="icon-notification" style={errorIconStyle} />
+
+            errorMessage = <div className="errorMessage">Oops!<div style={leftArrowStyle} /></div>;
+        }
+
+        return (<div className="login">
+            <div className={gradientClass} style={{height: '3px'}}/>
             <div className="signinContent" style={{height: '100%', padding: '16px'}}>
-                <div className="username" style={usernameStyle}>
+                {errorMessage}
+                <div className="username">
                     <div style={indicatorStyle}>
-                        <div className="icon-user" style={iconStyle} />
+                        {indicatorIcon}
                     </div>
-                    <input style={usernameInputStyle} placeholder="Username" />
+                    <input placeholder="Username" />
                 </div>
                 <div className="password" style={{borderRadius: '2px'}}>
                     <div style={passwordIndicatorStyle}>
                         <div className="icon-key" style={iconStyle} />
                     </div>
-                    <input style={passwordInputStyle} placeholder="Password" />
-                    <div style={submitButtonStyle}>
-                        <div className="icon-arrow-right" style={iconStyle} />
+                    <input placeholder="Password" />
+                    <div className="submitButton" onClick={this.clickHandler}>
+                        {buttonIcon}
                     </div>
                 </div>
             </div>
-            <div style={subtextStyle}>
-                <span style={{fontSize: '16px'}}>SIGN IN</span>
-                <a style={forgotStyle}>forgot?</a>
+            <div className="subtext">
+                <span>SIGN IN</span>
+                <a>forgot?</a>
             </div>
-            <div style={arrowStyle} />
         </div>);
     }
 });
